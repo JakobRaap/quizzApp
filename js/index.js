0230249a5a787b9a8d59db38d89bab6ed8d340e7
Jakob Raap <bokaj.paar@gmail.com>
2023-03-17 09:40:49 +0100

    answer buttons toggle function added

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
-let answerButton = document.querySelector('[data-js="logging-button"]');
-let answer = document.getElementById("answer_one");
-answer.add;
+const answerButtonOne = document.querySelector('[data-js="answer-button-one"]');
+const answerOne = document.querySelector('[data-js="answer_one"]');
+const answerButtonTwo = document.querySelector('[data-js="answer-button-two"]');
+const answerButtonThree = document.querySelector(
+  '[data-js="answer-button-three"]'
+);
+const answerTwo = document.querySelector('[data-js="answer_two"]');
+const answerThree = document.querySelector('[data-js="answer_three"]');
+
+answerButtonOne.addEventListener("click", () => {
+  answerOne.classList.toggle("qcard__answer--hide");
+});
+
+answerButtonTwo.addEventListener("click", () => {
+  answerTwo.classList.toggle("qcard__answer--hide");
+});
+
+answerButtonThree.addEventListener("click", () => {
+  answerThree.classList.toggle("qcard__answer--hide");
+});
